82b0180c68cfa27a72c1e8df5101b5ee3ac49dfe
roronya <roronya628@gmail.com>
2025-06-17 14:08:03 +0900

    init spring-ai-rag

---
+++ spring-ai-rag/src/test/java/org/example/springairag/SpringAiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springairag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
